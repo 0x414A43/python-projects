0c3c3691908d86d3ba188776f7169fbb6c6bd4f1
Andrew <andrew.j.cabrera@gmail.com>
2021-02-05 22:56:22 +1030

    commit

---
+++ python-speed-course.py
@@ -0,0 +1,27 @@
+## https://www.youtube.com/watch?v=VchuKL44s6E
+## Upto: 7:30/1:19:40
+
+#Data Types
+#Int
+-234234
+234234
+#Float
+2342.0
+-9.2
+#String
+'4.6'
+"hello"
+#Bool
+True
+False
+
+
+#Output and printing
+print('Hello World')
+print('4.5', "Hello")
+
+print("Hello", 'end', 87, False, end='\n')
+
+print("Hello", 'end', 87, False, end='|')
+print('Hello')
+
